d699193d82535e14738ebeb1241ab30cfdc8400f
Misza Nowak <mnowak@intechhouse.com>
2023-05-16 13:56:17 +0200

    server

---
+++ routes/root.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+const path = require("path");
+
+// regex to match the root path, or index or index.html
+router.get("^/$ |/index(.html)? ", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "views", "index.html"));
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 3501;
+const path = require("path");
+
+// __dirname = current directory, help to lok for static files
+app.use("/", express.static(path.join(__dirname, "/public")));
+
+app.use("/", require("./routes/root"));
+
+app.all("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ message: "404 Not found" });
+  } else {
+    res.type("txt").send("404 Not found");
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
